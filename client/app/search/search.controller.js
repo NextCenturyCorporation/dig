a5097c3cbb14ec3654ded069303571b73e086cbb
Rachel Artiss <rachel.artiss@nextcentury.com>
2014-12-10 17:25:03 -0500

    Initial check in of search list view and details page.

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -1,6 +1,57 @@
 'use strict';
 
-angular.module('digApp')
-  .controller('SearchCtrl', function () {
+var app = angular.module('digApp');
 
-  });
+app.controller('SearchCtrl', ['$scope', '$state', function ($scope,  $state) {
+
+  $scope.imageChecked = false;
+  $scope.currentOpened = 0;
+  $scope.selectedImage = 0;
+  $scope.thumbClass = "col-md-0";
+  $scope.descriptionClass = "col-md-12";
+
+  $scope.closeOthers = function(index, array) {
+    if($scope.currentOpened < array.length) {
+      array[$scope.currentOpened].isOpen = false;
+    }
+    $scope.currentOpened = index;
+  }
+
+  $scope.imageCheckedClick = function() {
+    $scope.imageChecked = !$scope.imageChecked;
+    
+    if($scope.imageChecked) {
+      $scope.thumbClass = "col-md-2";
+      $scope.descriptionClass = "col-md-10";
+    } else {
+      $scope.thumbClass = "col-md-0";
+      $scope.descriptionClass = "col-md-12";
+    }
+  }
+  
+	$scope.viewDetails = function(doc) {
+		$scope.doc = doc;
+		$state.go('search.list.details');
+	}
+
+	$scope.viewList = function() {
+    if($scope.doc) {
+      $scope.doc = null; 
+    }
+		$state.go('search.list');
+	}
+
+  $scope.selectImage = function(index) {
+    $scope.selectedImage = index;
+  }
+  
+  // Temporary until ad body text is cleaned up
+	$scope.cleanString = function(str) {
+		return str.replace(/[^a-z0-9\-\!\@\#\$\%\^\&\*\(\)\s\.\,\'\"']/gi, '');
+	}
+
+	if($state.current.name === 'search') {
+		$scope.viewList();
+	}
+
+}]);
--- client/app/search/search.js
+++ client/app/search/search.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('digApp')
+
   .config(function ($stateProvider) {
     $stateProvider
       .state('search', {
         url: '/',
         templateUrl: 'app/search/search.html',
         controller: 'SearchCtrl'
-      });
+      })
+      .state('search.list', {
+        url: '^/list',
+        templateUrl: 'app/search/search.list.html', 
+        controller: 'SearchCtrl'
+      })
+      .state('search.list.details', {
+        url: '^/list/details',
+        templateUrl: 'app/search/search.list.details.html', 
+        controller: 'SearchCtrl'
+      })
   });
