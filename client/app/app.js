e920f3b21d2f0a1ca62854d7fdc59a7d83808700
Josh Williams <halfs13@gmail.com>
2015-01-07 09:52:28 -0500

    DIG-88 basing euiHost cofigs off environment variables and serving at /config; Angular pulls config before bootstrapping

--- client/app/app.js
+++ client/app/app.js
@@ -1,20 +1,31 @@
 'use strict';
 
-angular.module('digApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'btford.socket-io',
-  'ui.router',
-  'ui.bootstrap',
-  'elasticui'
+var digApp = angular.module('digApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'btford.socket-io',
+    'ui.router',
+    'ui.bootstrap',
+    'elasticui'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
-      .otherwise('/');
+        .otherwise('/');
 
     $locationProvider.html5Mode(true);
-  })
+});
 
-  // TODO: find a nice way to collect constants for different environments
-  .constant('euiHost', 'http://localhost:9200');
+angular.element(document).ready(function() {
+    var $http = angular.injector(['ng']).get('$http');
+
+    $http.get('/config')
+    .success(function(config) {
+        var euiHost = (config.euiHost || 'localhost') + ':' + (config.euiPort || 9200);
+        digApp.constant('euiHost', euiHost);
+        angular.bootstrap(document, ['digApp']);
+    })
+    .error(function() {
+        digApp.constant('euiHost', 'http://localhost:9200');
+    });
+});
--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -13,34 +13,36 @@ function requiredProcessEnv(name) {
 // All configurations will extend these options
 // ============================================
 var all = {
-  env: process.env.NODE_ENV,
+    env: process.env.NODE_ENV,
 
-  // Root path of server
-  root: path.normalize(__dirname + '/../../..'),
+    // Root path of server
+    root: path.normalize(__dirname + '/../../..'),
 
-  // Server port
-  port: process.env.PORT || 9000,
+    // Server port
+    port: process.env.PORT || 9000,
 
-  // Should we populate the DB with sample data?
-  seedDB: false,
+    // Should we populate the DB with sample data?
+    seedDB: false,
 
-  // Secret for session, you will want to change this and make it an environment variable
-  secrets: {
-    session: 'dig-secret'
-  },
+    // Secret for session, you will want to change this and make it an environment variable
+    secrets: {
+        session: 'dig-secret'
+    },
 
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
+    // List of user roles
+    userRoles: ['guest', 'user', 'admin'],
 
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
+    // MongoDB connection options
+    mongo: {
+        options: {
+            db: {
+                safe: true
+            }
+        }
+    },
 
+    euiServerUrl: process.env.EUI_SERVER_URL || 'http://localhost',
+    euiServerPort: process.env.EUI_SERVER_PORT || 9200
 };
 
 // Export the config object based on the NODE_ENV
