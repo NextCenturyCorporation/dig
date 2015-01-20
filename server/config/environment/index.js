ec0bffdfe90c80e86cfe2ddde3f949fede61aa38
Josh Williams <halfs13@gmail.com>
2015-01-20 07:17:43 -0500

    DIG-230 adding css blur as fallback to js pixelation and option for blur or pixelate from the server side

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -48,9 +48,10 @@ var all = {
     imageSimUrl: process.env.IMAGE_SIM_URL || 'http://localhost',
     imageSimPort: process.env.IMAGE_SIM_PORT || 3001,
 
-    blurImages: ((!!process.env.BLUR_IMAGES && process.env.BLUR_IMAGES === 'false') ? false : true),
+    blurImages: ((!!process.env.BLUR_IMAGES && process.env.BLUR_IMAGES === 'false') ? false : (process.env.BLUR_IMAGES ? process.env.BLUR_IMAGES : 'blur')),
 
-    blurPercentage: process.env.BLUR_PERCENT || 5
+    blurPercentage: process.env.BLUR_PERCENT || 2.5,
+    pixelatePercentage: process.env.PIXELATE_PERCENT || 5
 };
 
 // Export the config object based on the NODE_ENV
