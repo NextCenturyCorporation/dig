b7601ba3f95fbb653000807843f3fb3c96d029c0
lmizenextcen <lawerence.mize@nextcentury.com>
2015-01-22 18:45:28 -0500

    Reworked the aggregation checklists to always display any previously checked items when aggregation buckets change.  Updated the paging logic to handle this and styled the +/-/All buttons.

--- client/components/checkbox-filter/checkbox-filter-list/checkbox-filter-list.directive.js
+++ client/components/checkbox-filter/checkbox-filter-list/checkbox-filter-list.directive.js
@@ -15,41 +15,102 @@ angular.module('digApp.directives')
             filterStates: '='
         },
         templateUrl: 'components/checkbox-filter/checkbox-filter-list/checkbox-filter-list.partial.html',
-        link: function($scope) {
+        link: function($scope, $element) {
             $scope.facetCount = 0;
+            $scope.showAll = false;
+            $scope.displayCount = $scope.searchCount;
+            $scope.displayBuckets = [];
+
             $scope.buttonStatus = {
                 more: false,
                 less: false,
                 all: false
             };
 
+            $element.addClass('checkbox-filter-list');
+
             $scope.more = function() {
-                $scope.searchCount = $scope.facetCount + 10;
+                $scope.displayCount = $scope.displayCount + 10;
+                $scope.resetDisplayBuckets();
             };
 
             $scope.less = function() {
-                if($scope.facetCount % 10 === 0) {
-                    $scope.searchCount = $scope.facetCount - 10;
-                } else if($scope.facetCount > 20) {
-                    $scope.searchCount = $scope.facetCount - (10 + ($scope.facetCount % 10));
-                } else {
-                    $scope.searchCount = $scope.facetCount - ($scope.facetCount % 10);
+                $scope.displayCount = $scope.displayCount - 10;
+                if ($scope.displayCount < 10) {
+                    $scope.displayCount = 10;
                 }
+                $scope.resetDisplayBuckets();
             };
 
             $scope.all = function() {
-                $scope.searchCount = 0;
+                if ($scope.showAll) {
+                    $scope.displayCount = $scope.lastDisplayCount;
+                } else {
+                    $scope.lastDisplayCount = $scope.displayCount;
+                    $scope.displayCount = (Math.floor($scope.facetCount / 10) * 10) + 10;
+                }
+                $scope.showAll = !$scope.showAll;
+
+                $scope.resetDisplayBuckets();
             };
 
             var disableButtons = function() {
-                console.log(($scope.searchCount === $scope.facetCount));
-                $scope.buttonStatus.moreButton = ($scope.searchCount === $scope.facetCount);
-                $scope.buttonStatus.lessButton = ($scope.searchCount > 10 || $scope.searchCount === 0);
-                $scope.buttonStatus.allButton = ($scope.searchCount === $scope.facetCount);
+                $scope.buttonStatus.moreButton = ($scope.displayCount === $scope.facetCount);
+                $scope.buttonStatus.lessButton = ($scope.displayCount > 10 || $scope.displayCount === 0);
+                $scope.buttonStatus.allButton = ($scope.displayCount === $scope.facetCount);
             };
+
+            $scope.getAggregationTermBucket = function(term) {
+                var aggObj = null;
+
+                // Return false if we have no aggregations or none on that field.
+                if (!$scope.buckets) {
+                    return false;
+                }
+
+                aggObj = (_.filter($scope.buckets, function(bucket) {
+                        return (bucket.key && bucket.key == term);
+                    }));
+
+                return (aggObj.length !== 0) ? aggObj[0] : false;
+            };
+
+            $scope.resetDisplayBuckets = function() {
+                $scope.displayBuckets = [];
+                var checkedItems = [];
+                var bucket;
+                // Get checked items and add to display buckets if they dropped out of our aggregations.
+                for (var term in $scope.filterStates[$scope.aggregationName]) {
+                    if ($scope.filterStates[$scope.aggregationName].hasOwnProperty(term) &&
+                        $scope.filterStates[$scope.aggregationName][term]) {
+                        checkedItems.push(term);
+                        bucket = ($scope.getAggregationTermBucket(term) || {
+                            key: term,
+                            doc_count: 0
+                        });
+                        $scope.displayBuckets.push(bucket);
+                    }
+                }
+
+                // fill the rest of display buckets with the aggregation buckets.
+                var numBuckets = ($scope.buckets) ? $scope.buckets.length : 0;
+                var remainingBucketSlots = Math.min($scope.displayCount, numBuckets) - checkedItems.length;
+                var i = 0;
+
+                while (remainingBucketSlots > 0) {
+                    if (!_.contains(checkedItems, $scope.buckets[i].key)) {
+                       $scope.displayBuckets.push($scope.buckets[i]);
+                       remainingBucketSlots--;
+                    }
+                    i++;
+                }
+            }
 
             $scope.$watch('buckets', function() {
                 $scope.facetCount = ($scope.buckets ? $scope.buckets.length : 0);
+
+                $scope.resetDisplayBuckets();
+
                 disableButtons();
             }, true);
         }
