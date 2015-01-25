60c7945b2485b3577254ce6d85ccc560d58d0f63
lmizenextcen <lawerence.mize@nextcentury.com>
2015-01-25 15:23:33 -0500

    Changing the logic for selected aggregation buckets.   Reinstated the bounds on the number of aggregation buckets returned from the server.  Also, if a facet term has been selected but was not returned in the subsequent set of aggregation buckets, we continue to render in the page so the eui directives stay consistent.  By default, these will remain hidden from the user until they hit the more (+) button enough to allow those terms to return naturally at their appropriate ranked position
    This eliminates the need to ask the server for nearly all the possible aggregation values with doc counts >= 1.

--- client/components/checkbox-filter/checkbox-filter-list/checkbox-filter-list.directive.js
+++ client/components/checkbox-filter/checkbox-filter-list/checkbox-filter-list.directive.js
@@ -7,7 +7,7 @@ angular.module('digApp.directives')
         scope: {
             aggregationName: '@',
             aggregationKey: '@',
-            searchCount: '=',
+            userDisplayCount: '=searchCount',
             buckets: '=',
             indexVM: '=indexvm',
             ejs: '=',
@@ -18,7 +18,6 @@ angular.module('digApp.directives')
         link: function($scope, $element) {
             $scope.facetCount = 0;
             $scope.showAll = false;
-            $scope.userDisplayCount = -1;
             $scope.displayBuckets = [];
 
             $scope.buttonStatus = {
@@ -30,7 +29,7 @@ angular.module('digApp.directives')
             $element.addClass('checkbox-filter-list');
 
             $scope.more = function() {
-                if($scope.userDisplayCount < $scope.facetCount) {
+                if($scope.userDisplayCount <= $scope.facetCount) {
                     $scope.userDisplayCount += 10;
                     $scope.resetDisplayBuckets();
                 }
@@ -61,7 +60,7 @@ angular.module('digApp.directives')
             };
 
             var disableButtons = function() {
-                $scope.buttonStatus.moreButton = ($scope.userDisplayCount >= $scope.facetCount);
+                $scope.buttonStatus.moreButton = ($scope.userDisplayCount > $scope.facetCount);
                 $scope.buttonStatus.lessButton = ($scope.userDisplayCount <= 10);
                 $scope.buttonStatus.allButton = ($scope.facetCount <= 10);
             };
@@ -88,35 +87,39 @@ angular.module('digApp.directives')
                 var checkedItems = [];
                 var bucket;
 
-                // Reset display count if necessary.
-                $scope.userDisplayCount = ($scope.userDisplayCount < 0) ? $scope.searchCount : $scope.userDisplayCount;
-
                 // Get checked items and add to display buckets if they dropped out of our aggregations.
                 for (var term in $scope.filterStates[$scope.aggregationName]) {
                     if ($scope.filterStates[$scope.aggregationName].hasOwnProperty(term) &&
                         $scope.filterStates[$scope.aggregationName][term]) {
                         checkedItems.push(term);
-                        /* jshint camelcase:false */
-                        bucket = ($scope.getAggregationTermBucket(term) || {
-                            key: term,
-                            doc_count: 0
-                        });
-                        /* jshint camelcase:true */
-                        $scope.displayBuckets.push(bucket);
                     }
                 }
 
                 // fill the rest of display buckets with the aggregation buckets.
                 var numBuckets = ($scope.buckets) ? $scope.buckets.length : 0;
-                var remainingBucketSlots = Math.min($scope.userDisplayCount, numBuckets) - checkedItems.length;
-                var i = 0;
+                var remainingBucketSlots = Math.min($scope.userDisplayCount, numBuckets);
 
-                while (remainingBucketSlots > 0) {
-                    if (!_.contains(checkedItems, $scope.buckets[i].key)) {
-                       $scope.displayBuckets.push($scope.buckets[i]);
-                       remainingBucketSlots--;
+                for (var i = 0; i < remainingBucketSlots; i++) {
+                    $scope.displayBuckets.push($scope.buckets[i]);
+                }
+
+                // Add hidden fields for any checked items that didn't come
+                // back in our current aggregation set.  This is necessary to
+                // keep the eui directives in sync with the user state.  If no
+                // controll for this these are in the page, it will result in 
+                // extraneous filters being applied to elastic query searches.
+                for (i = 0; i < checkedItems.length; i++) {
+                    if (!_.find($scope.displayBuckets, function(item) {
+                        return item.key === checkedItems[i];
+                    })) {
+                        /* jshint camelcase:false */
+                        $scope.displayBuckets.push({
+                            key: checkedItems[i],
+                            doc_count: 0,
+                            hidden: true
+                        });
+                        /* jshint camelcase:true */
                     }
-                    i++;
                 }
             };
 
