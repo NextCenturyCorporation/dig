c18a5e9386ef464203cbd6d7753cca89b7494d06
Josh Williams <halfs13@gmail.com>
2015-02-02 08:53:14 -0500

    DIG-161 cleaning up and adding css for gallery

--- client/app/search/search-results/gallery/gallery.controller.js
+++ client/app/search/search-results/gallery/gallery.controller.js
@@ -10,27 +10,6 @@ angular.module('digApp')
         rawData: []
     };
 
-    var getGalleryData = function() {
-        var galleryData = [];
-        var rowData;
-        var displayCount = ($scope.indexVM.results && $scope.indexVM.results.hits ? $scope.indexVM.results.hits.hits.length : 0);
-
-        var i;
-        var j;
-        var dataIndex;
-        for(i = 0; i < (displayCount / $scope.counts.row); i++) {
-            rowData = [];
-            for(j = 0; j < $scope.counts.row; j++) {
-                dataIndex = (i * $scope.counts.row) + j;
-
-                rowData.push($scope.indexVM.results.hits.hits[dataIndex]);
-            }
-            galleryData.push(rowData);
-        }
-
-        return galleryData;
-    };
-
     $scope.renderExpander = function(clickEvent) {
         var el = clickEvent.target;
 
@@ -68,8 +47,6 @@ angular.module('digApp')
     };
 
     $scope.$watch('indexVM.results', function() {
-        console.log("firing data processing");
         $scope.gallery.rawData = ($scope.indexVM.results && $scope.indexVM.results.hits ? $scope.indexVM.results.hits.hits : []);
-        $scope.gallery.formattedData = getGalleryData();
     }, true);
 });
