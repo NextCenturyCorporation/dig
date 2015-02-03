9c0d6df0b53688a8a4560003bdb886adf9c1a997
Josh Williams <halfs13@gmail.com>
2015-02-03 14:41:30 -0500

    DIG-203 reworking gallery dom manipulation from controller to directive

--- client/app/search/search-results/gallery/gallery.controller.js
+++ client/app/search/search-results/gallery/gallery.controller.js
@@ -6,42 +6,6 @@ angular.module('digApp')
         rawData: []
     };
 
-    $scope.renderExpander = function(clickEvent) {
-        var el = clickEvent.target;
-
-        $('#gallery-expander').remove();
-
-        var parentEl = getParentEl(el);
-        var docNum = parentEl[0].attributes['gallery-index'].value;
-        var insertAfterEl = findRowLastEl(parentEl);
-
-        insertAfterEl.after($compile('<expanded-listing-view id="gallery-expander" doc="indexVM.results.hits.hits[' + docNum + ']" '+
-            'get-display-image-src="getDisplayImageSrc" view-details="viewDetails" parent-state="gallery"></expanded-listing-view>')($scope));
-    };
-
-    var getParentEl = function(el) {
-        var parentDiv = $(el).parent().closest('div.gallery-img-container');
-        return parentDiv;
-    };
-
-    var findRowLastEl = function(parentDiv) {
-        var y = parentDiv.position().top;
-
-        var currentEl = parentDiv;
-        var nextEl;
-        var lastEl;
-        while(!lastEl) {
-            nextEl = currentEl.next('div.gallery-img-container');
-            if(nextEl.length === 0 || (nextEl.position().top > y)) {
-                //currentEl is last el or last in row
-                lastEl = currentEl;
-            } else {
-                currentEl = nextEl;
-            }
-        }
-        return lastEl;
-    };
-
     $scope.$watch('indexVM.results', function() {
         $scope.gallery.rawData = ($scope.indexVM.results && $scope.indexVM.results.hits ? $scope.indexVM.results.hits.hits : []);
     }, true);
