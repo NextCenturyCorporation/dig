a08d83ce8960a0a448e30b3f5264e15babb3e9d5
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-02-03 17:51:40 -0500

    Adding unit tests.

--- client/app/blurImageService/blurImageService.service.js
+++ client/app/blurImageService/blurImageService.service.js
@@ -1,30 +1,34 @@
 'use strict';
 
-angular.module('digApp.services')
+angular.module('digApp')
 .service('blurImageService', function(blurImagesEnabled, blurImagesPercentage, pixelateImagesPercentage) {
     var blurConfig = this;
 
     blurConfig.getBlurImagesEnabled = function() {
-        return sessionStorage.blurImagesEnabled || blurImagesEnabled;
+        return (sessionStorage.blurConfig ? JSON.parse(sessionStorage.blurConfig).blurImagesEnabled : blurImagesEnabled);
     };
 
     blurConfig.getBlurImagesPercentage = function() {
-        return sessionStorage.blurImagesPercentage || blurImagesPercentage;
+        return (sessionStorage.blurConfig ? JSON.parse(sessionStorage.blurConfig).blurImagesPercentage : blurImagesPercentage);
     };
 
     blurConfig.getPixelateImagesPercentage = function() {
-        return sessionStorage.pixelateImagesPercentage || pixelateImagesPercentage;
+        return (sessionStorage.blurConfig ? JSON.parse(sessionStorage.blurConfig).pixelateImagesPercentage : pixelateImagesPercentage);
     };
 
     blurConfig.changeBlurImagesEnabled = function(isBlurred) {
+        var tempObject = {};
+        
         if(isBlurred) {
-            sessionStorage.blurImagesEnabled = blurImagesEnabled ? blurImagesEnabled : 'blur';
-            sessionStorage.blurImagesPercentage = (blurImagesPercentage > 0) ? blurImagesPercentage : 2.5;
-            sessionStorage.pixelateImagesPercentage = (pixelateImagesPercentage > 0) ? pixelateImagesPercentage : 2.5;
+            tempObject.blurImagesEnabled = blurImagesEnabled ? blurImagesEnabled : 'blur';
+            tempObject.blurImagesPercentage = (blurImagesPercentage > 0) ? blurImagesPercentage : 2.5;
+            tempObject.pixelateImagesPercentage = (pixelateImagesPercentage > 0) ? pixelateImagesPercentage : 5;
+            sessionStorage.blurConfig = JSON.stringify(tempObject);
         } else {
-            sessionStorage.blurImagesEnabled = blurImagesEnabled ? false : blurImagesEnabled;
-            sessionStorage.blurImagesPercentage = (blurImagesPercentage === 0) ? blurImagesPercentage : 0;
-            sessionStorage.pixelateImagesPercentage = (pixelateImagesPercentage === 0) ? pixelateImagesPercentage : 0;
+            tempObject.blurImagesEnabled = blurImagesEnabled ? false : blurImagesEnabled;
+            tempObject.blurImagesPercentage = (blurImagesPercentage === 0) ? blurImagesPercentage : 0;
+            tempObject.pixelateImagesPercentage = (pixelateImagesPercentage === 0) ? pixelateImagesPercentage : 0;
+            sessionStorage.blurConfig = JSON.stringify(tempObject);
         }
     };
 
