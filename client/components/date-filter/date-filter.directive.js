41e0f279abba5c55b70669b57e6a5588f02f7c9a
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-03-02 14:21:12 -0500

    Initial commit for date filtering functionality (DIG-341).

---
+++ client/components/date-filter/date-filter.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('digApp.directives')
+.directive('dateFilter', function($filter) {
+    return {
+        restrict: 'E',
+        scope: {
+            indexVM: '=indexvm',
+            ejs: '=',
+            filters: '='
+        },
+        templateUrl: 'components/date-filter/date-filter.partial.html',
+        link: function($scope) {
+            $scope.startDate = null;
+            $scope.endDate = null;
+            $scope.dateFormat = 'yyyy-MM-dd';
+
+            $scope.dateToString = function(date) {
+                return $filter('date')(date, $scope.dateFormat);
+            };
+        }
+    };
+});
---
+++ client/components/date-filter/datepicker-wrapper/datepicker-wrapper.directive.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('digApp.directives')
+.directive('datepickerWrapper', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            dateLabel: '@',
+            date: '=',
+            format: '='
+        },
+        templateUrl: 'components/date-filter/datepicker-wrapper/datepicker-wrapper.partial.html',
+        link: function($scope) {
+
+            $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1
+            };
+
+            $scope.open = function($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.opened = true;
+            };
+
+            $scope.clear = function () {
+                $scope.date = null;            
+            };
+        }
+    };
+});
