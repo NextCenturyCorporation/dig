1d69772baee19d286244a3250bfb2fa4293baee0
Josh Williams <halfs13@gmail.com>
2015-03-19 09:25:33 -0400

    DIG-324 styling missing checkbox and making only apply when another filter is set

--- client/components/checkbox-filter/checkbox-filter.directive.js
+++ client/components/checkbox-filter/checkbox-filter.directive.js
@@ -18,6 +18,26 @@ angular.module('digApp.directives')
         link: function($scope) {
             $scope.filterStates[$scope.aggregationName] = $scope.filterStates[$scope.aggregationName] || {};
             $scope.aggregationCount = $scope.aggregationCount() || 30;
+
+            var checkAnyFiltersActive = function() {
+                var filterState = $scope.filterStates[$scope.aggregationName];
+                for(var key in filterState) {
+                    if(key !== '__include_missing' &&!!filterState[key]) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            $scope.activeFilters = {
+                filtersAreSet: checkAnyFiltersActive()
+            };
+
+            $scope.$watch(function() {
+                return $scope.filterStates[$scope.aggregationName];
+            }, function() {
+                $scope.activeFilters.filtersAreSet = checkAnyFiltersActive();
+            }, true);
         }
     };
 });
