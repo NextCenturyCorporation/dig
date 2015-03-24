e59af34e8e5f0ab338806da953807cc7858c323a
Josh Williams <halfs13@gmail.com>
2015-03-24 14:45:53 -0400

    DIG-365 styling

--- client/app/search/search-error/search-error.controller.js
+++ client/app/search/search-error/search-error.controller.js
@@ -4,6 +4,13 @@ angular.module('digApp')
 .controller('SearchErrorCtrl', function($scope) {
     $scope.error = null;
 
+    $scope.getTitle = function() {
+        if(!$scope.error) {
+            parseError();
+        }
+        return $scope.error.title;
+    };
+
     $scope.getErrorMessage = function() {
         if(!$scope.error) {
             parseError();
@@ -12,20 +19,28 @@ angular.module('digApp')
     };
 
     var parseError = function() {
+        $scope.error = {
+            message: '',
+            title: '',
+            showDetails: false
+        };
+
         if($scope.indexVM.error) {
             if($scope.indexVM.error.message) {
-                $scope.error = {
-                    message: $scope.indexVM.error.message
-                };
+                $scope.error.message = $scope.indexVM.error.message;
             } else {
-                $scope.error = {
-                    message: $scope.indexVM.error
-                };
+                $scope.error.message = $scope.indexVM.error;
             }
-        } else {
-            $scope.error = {
-                message: ''
-            };
+
+            if($scope.indexVM.error.body && $scope.indexVM.error.body.status) {
+                if($scope.indexVM.error.body.status === 400 && $scope.indexVM.error.message.indexOf('Parse Failure') > -1) {
+                    $scope.error.title = 'Search Parse Error';
+                }
+            } else {
+                if($scope.indexVM.error.message === 'No Living connections') {
+                    $scope.error.title = 'Cannot Connect to ElasticSearch';
+                }
+            }
         }
     };
 });
