867f2b25c44120a6c6d0c6f7aca87053f2a9296f
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-04-07 14:05:01 -0400

    Remove socket for query collection.

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,7 +18,6 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
-  require('../api/query/query.socket').register(socket);
   //require('../api/thing/thing.socket').register(socket);
 }
 
