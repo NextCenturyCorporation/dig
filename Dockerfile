4762a5694870bf66091e31b05c253edaec39ac61
Andrew Bovill <andrew.bovill@nextcentury.com>
2015-04-07 15:09:15 -0400

    Fixed dockerfile for digapp Fixed package script fixed docker-compose config file

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
-FROM ubuntu
-RUN sudo apt-get install -y curl
-RUN curl -sL https://deb.nodesource.com/setup | sudo bash -
-RUN sudo apt-get install -y nodejs
-RUN sudo npm update -g npm
-RUN sudo apt-get install -y git
-RUN mkdir -p /usr/local/share/dig
-COPY dist /usr/local/share/dig
-WORKDIR /usr/local/share/dig
-RUN npm cache clean
-RUN npm install
-RUN sudo npm install -g forever
-EXPOSE 8080
-CMD ["forever", "/usr/local/share/dig/server/app.js"]
+FROM node:0.12.2
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ONBUILD COPY package.json /usr/src/app/
+ONBUILD RUN npm install
+ONBUILD COPY . /usr/src/app
+
+CMD [ "npm", "start" ]
--- package.sh
+++ package.sh
@@ -5,34 +5,19 @@ DOCKER_COMPOSE_FILE=docker-compose.yml
 DEFAULT_INSTALL_PATH=/usr/local/dig
 
 TEMP_DIR=$(mktemp -d)
-
-cat > ${TEMP_DIR}/bootstrap.sh <<EOF
-#!/bin/bash
-
-read -p "Where should dig be installed? [${DEFAULT_INSTALL_PATH}]: " input
-echo "Using '${input:=$DEFAULT_INSTALL_PATH}' as install path for dig"
-
-mkdir -p ${input}
-cp -r . ${input}
-rm ${input}/bootstrap.sh
-
-cd ${input}
-curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-
-EOF
+cp bootstrap.sh ${TEMP_DIR}
 
 cat > ${TEMP_DIR}/run.sh <<EOF
 #!/bin/bash
-cd dist/
-../docker-compose -f ../docker-compose.yml up
+./docker-compose up
 EOF
 chmod +x ${TEMP_DIR}/bootstrap.sh
 chmod +x ${TEMP_DIR}/run.sh
 cp -r dist/ ${TEMP_DIR}
+cp -r conf/ ${TEMP_DIR}
 cp docker-compose.yml ${TEMP_DIR}
 
-makeself ${TEMP_DIR} dig_deploy.sh "Deployment package for DIG" ./bootstrap.sh
+makeself --notemp ${TEMP_DIR} dig_deploy.sh "Deployment package for DIG" ./bootstrap.sh
 
 while getopts ":k" opt; do
     case $opt in
