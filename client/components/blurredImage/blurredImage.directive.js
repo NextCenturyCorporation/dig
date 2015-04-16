0dd5f8c251f8da4ab22eb0bef07e510e19a25325
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-04-16 19:07:24 -0400

    Fix for performance issues.

--- client/components/blurredImage/blurredImage.directive.js
+++ client/components/blurredImage/blurredImage.directive.js
@@ -6,7 +6,6 @@ angular.module('digApp.directives')
     return {
         restrict: 'A',
         link: function($scope, element, attrs) {
-            var processing = false;
             var fallback = false;
             var blurImagesEnabled = blurImageService.getBlurImagesEnabled();
             var blurImagesPercentage = blurImageService.getBlurImagesPercentage();
@@ -49,111 +48,28 @@ angular.module('digApp.directives')
                 });
             };
 
-            var bindErrorHandler = function(img) {
-                if(img.addEventListener) {
-                    img.addEventListener('error', function (e) {
-
-                        $scope.$apply(function() {
-                            $scope.$watch($scope.getMaxSize, function() {
-                                cssBlur();
-                            });
-                            fallback = true;
-                            processing = null;
-                            element.removeClass('hide-preblur-image');
-                            $timeout(cssBlur);
-                            e.preventDefault(); // Prevent error from getting thrown
-                        });
-                    });
-                } else {
-                    // Old IE uses .attachEvent instead
-                    img.attachEvent('onerror', function (e) {
-                        $scope.$watch($scope.getMaxSize, function() {
-                            cssBlur();
-                        });
-                        fallback = true;
-                        element.removeClass('hide-preblur-image');
-                        processing=null;
-                        $scope.$watch('getMaxSize', cssBlur);
-                        $timeout(cssBlur);
-                        return false; // Prevent propagation
-                    });
-                }
-            };
-
-            var pixelate = function(imageSource) {
-                element.addClass('hide-preblur-image');
-
-                var canvas = document.createElement('canvas');
-                var ctx = canvas.getContext('2d');
-                var img = new Image();
-                img.crossOrigin = 'Anonymous';
-
-                processing = img;
-
-                img.onload = function() {
-                    canvas.height = img.height;
-                    canvas.width = img.width;
-                    ctx.drawImage(img,0,0);
-
-                    var pixelateSize = (Math.max(img.height, img.width) * (pixelateImagesPercentage / 100));
-
-                    if(blurImagesEnabled === 'pixelate' && !fallback) {
-                        var imgStr = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-
-                        JSManipulate.pixelate.filter(imgStr, {
-                            size: pixelateSize
-                        });
-
-                        ctx.putImageData(imgStr,0,0);
-
-                        var imgStr2 = canvas.toDataURL('image/png');
-
-                        element.attr('src', imgStr2);
-                        element.removeClass('hide-preblur-image');
-                    } else {
-                        cssBlur();
-                    }
-
-                    processing = null;
-                };
-
-                bindErrorHandler(img);
-
-                img.src = imageSource;
-            };
-
-            var blurImage = function(imageSource) {
-                if(!fallback && blurImagesEnabled === 'pixelate') {
-                    pixelate(imageSource);
-                } else if(!fallback && blurImagesEnabled === 'blur') {
+            var blurImage = function() {
+                if(!fallback && blurImagesEnabled === 'blur') {
                     fallback = true;
 
                     $scope.$watch($scope.getMaxSize, function() {
                         cssBlur();
                     });
-
-                    $timeout(cssBlur);
                 } else if(fallback || blurImagesEnabled === 'blur') {
                     $timeout(cssBlur);
                 }
             };
 
-            $scope.processImageBlur = function(imageSource) {
+            $scope.processImageBlur = function() {
                 blurImagesEnabled = blurImageService.getBlurImagesEnabled();
                 blurImagesPercentage = blurImageService.getBlurImagesPercentage();
                 pixelateImagesPercentage = blurImageService.getPixelateImagesPercentage();
 
-                if(!processing) {
-                    blurImage(imageSource);
-                } else {
-                    processing.onload = null; //cancel the onload
-                    blurImage(imageSource);
-                }
+                blurImage();
             };
 
-            attrs.$observe('src', function(imageSource) {
-                $scope.processImageBlur(imageSource);
+            attrs.$observe('src', function() {
+                $scope.processImageBlur();
             });
 
             $scope.$on('blur-state-change', function() {
