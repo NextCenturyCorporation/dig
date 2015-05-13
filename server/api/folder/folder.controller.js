47c257f9a378a451f4ef143dcc35ad7a61e370bd
Neva Reed <neva.reed@nextcentury.com>
2015-05-13 17:01:06 -0400

    DIG-543 Added delete, create, and modify endpoints for folder

--- server/api/folder/folder.controller.js
+++ server/api/folder/folder.controller.js
@@ -45,6 +45,7 @@ var folders = [
         childIds: []
       }
     ];
+var nextIndex = 7;
 
 // Get list of folders based on user
 exports.index = function(req, res) {
@@ -61,21 +62,123 @@ exports.show = function(req, res) {
     return res.send(404);
   }
 
-  if(req.params.id == 0) {
-    return res.json(folders[0]);
-  } else if(req.params.id == 1) {
-    return res.json(folders[1]);
-  } else if(req.params.id == 2) {
-    return res.json(folders[2]);
-  } else if(req.params.id == 3) {
-    return res.json(folders[3]);
-  } else if(req.params.id == 4) {
-    return res.json(folders[4]);
-  } else if(req.params.id == 5) {
-    return res.json(folders[5]);
-  } else if(req.params.id == 6) {
-    return res.json(folders[6]);
-  }else {
+  var id = parseInt(req.params.id);
+  var index = findIndex(id);
+
+  if(index == -1) {
     return res.send(404);
   }
+
+  return res.json(folders[index]);
 };
+
+// Creates a new folder in the DB
+exports.create = function(req, res) {
+  if(req.body.name == "ROOT" || req.headers.user !== "test") {
+    return res.send(404);
+  }
+
+  var index = folders.length;
+  folders[index] = req.body;
+  folders[index]._id = nextIndex;
+  var id = folders[index]._id;
+  nextIndex++;
+
+  // Add folder reference to parent
+  var parentIndex = findIndex(parseInt(req.body.parentId));
+  folders[parentIndex].childIds.push(id);
+
+  // Change children(s) parent reference (if any)
+  var children = folders[index].childIds;
+  for(var i = 0; i < children.length; i++) {
+    var childIndex = findIndex(children[i]);
+    if(childIndex != -1) {
+      // remove child reference in child's parent
+      var childOldParentIndex = findIndex(folders[childIndex].parentId);
+      folders[childOldParentIndex].childIds.splice(folders[childOldParentIndex].childIds.indexOf(children[i]), 1);
+
+      // change parent reference for good
+      folders[childIndex].parentId = id;
+    }
+  }
+
+  return res.json(201, folders[index]);
+};
+
+// Updates an existing folder in the DB
+exports.update = function(req, res) {
+  if(req.body.name == "ROOT" || req.headers.user !== "test") {
+    return res.send(404);
+  }
+
+  var id = parseInt(req.params.id);
+  var index = findIndex(id);
+
+  if(index == -1) {
+    return res.send(404);
+  } else if(folders[index].name == "ROOT") {
+    return res.send(404);
+  }
+
+  // remove folder reference in parent folder
+  var parentIndex = findIndex(folders[index].parentId);
+  folders[parentIndex].childIds.splice(folders[parentIndex].childIds.indexOf(index), 1);
+
+  folders[index].name = req.body.name;
+  folders[index].parentId = req.body.parentId;
+
+  // add folder reference in new parent folder
+  var newParentIndex = findIndex(folders[index].parentId);
+  folders[newParentIndex].childIds.push(id);
+
+  return res.json(200, folders[index]);
+};
+
+// Deletes a folder from the DB
+exports.destroy = function(req, res) {
+  if(req.headers.user !== "test") {
+    return res.send(404);
+  }
+
+  var id = parseInt(req.params.id);
+  var index = findIndex(id);
+
+  if(index == -1) {
+    return res.send(404);
+  } else if(folders[index].name == "ROOT") {
+    return res.send(404);
+  }
+
+  // remove folder reference in parent folder
+  var parentIndex = findIndex(folders[index].parentId);
+  folders[parentIndex].childIds.splice(folders[parentIndex].childIds.indexOf(index), 1);
+
+  // remove child folders recursively (if any)
+  removeChildren(folders[index].childIds)
+
+  folders.splice(index, 1);
+
+  return res.send(204);
+};
+
+function removeChildren(children) {
+  for(var i = 0; i < children.length; i++) {
+    var childIndex = findIndex(children[i]);
+    if(childIndex != -1) {
+      removeChildren(folders[childIndex].childIds);
+      folders.splice(childIndex, 1);
+    }
+  }
+}
+
+function findIndex(id) {
+  var index;
+
+  for(index = 0; index < folders.length; index++) {
+    if(folders[index]._id == id) {
+      return index;
+    }
+  }
+
+  return -1;
+}
