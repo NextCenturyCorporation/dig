191f436a16908104990d9d92c55e299310e04e2b
Neva Reed <neva.reed@nextcentury.com>
2015-05-15 14:26:57 -0400

    DIG-543 More tests, fixes, and comments to folders endpoint

--- server/api/folder/folder.controller.js
+++ server/api/folder/folder.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _ = require('lodash');
+
 var folders = [
       {
         _id: 0,
@@ -38,30 +39,44 @@ exports.show = function(req, res) {
 
 // Creates a new folder in the DB
 exports.create = function(req, res) {
-  if(req.body.name == "ROOT" || req.headers.user !== "test") {
+  // Prevents:
+  //    - creating folder with name ROOT
+  //    - creating in another user's folders
+  //    - adding itself as its parent
+  //    - adding itself as its child
+  if(req.body.name == "ROOT" || req.headers.user !== "test" ||
+      req.body.parentId == nextIndex || req.body.childIds.indexOf(nextIndex) != -1) {
     return res.send(404);
   }
 
+  var parentIndex = findIndex(parseInt(req.body.parentId));
+
+  // Prevent adding with unknown parent and adding folders within themselves
+  if(parentIndex == -1 || findIDInChildren(parentIndex, req.body.childIds)) {
+    return res.send(404);
+  }
+
+  // Create new folder with new _id
   var index = folders.length;
   folders[index] = req.body;
   folders[index]._id = nextIndex;
   var id = folders[index]._id;
   nextIndex++;
 
-  // Add folder reference to parent
-  var parentIndex = findIndex(parseInt(req.body.parentId));
+  // Add folder reference to its parent
   folders[parentIndex].childIds.push(id);
 
   // Change children(s) parent reference (if any)
   var children = folders[index].childIds;
   for(var i = 0; i < children.length; i++) {
+    // Get child's index in folder area
     var childIndex = findIndex(children[i]);
     if(childIndex != -1) {
-      // remove child reference in child's parent
+      // Remove this child's reference in its old parent
       var childOldParentIndex = findIndex(folders[childIndex].parentId);
       folders[childOldParentIndex].childIds.splice(folders[childOldParentIndex].childIds.indexOf(children[i]), 1);
 
-      // change parent reference for good
+      // Change parent reference in this child for good
       folders[childIndex].parentId = id;
     }
   }
@@ -71,6 +86,8 @@ exports.create = function(req, res) {
 
 // Updates an existing folder in the DB
 exports.update = function(req, res) {
+  // Prevents changing name to ROOT (or changing ROOT folder in general) and
+  // changing another user's folders
   if(req.body.name == "ROOT" || req.headers.user !== "test") {
     return res.send(404);
   }
@@ -78,20 +95,29 @@ exports.update = function(req, res) {
   var id = parseInt(req.params.id);
   var index = findIndex(id);
 
+  // Prevents:
+  //    - updating non-existant folder
+  //    - changing ROOT folder
+  //    - adding itself as its parent
+  //    - adding itself as its child
+  //    - moving itself to one of its subfolders
   if(index == -1) {
     return res.send(404);
-  } else if(folders[index].name == "ROOT") {
+  } else if(folders[index].name == "ROOT" ||
+      folders[index].parentId == id || folders[index].childIds.indexOf(id) != -1 ||
+      findIDInChildren(folders[index].parentId, folders[index].childIds)) {
     return res.send(404);
   }
 
-  // remove folder reference in parent folder
+  // Remove folder reference in its parent folder
   var parentIndex = findIndex(folders[index].parentId);
   folders[parentIndex].childIds.splice(folders[parentIndex].childIds.indexOf(index), 1);
 
+  // Update its name and parentID
   folders[index].name = req.body.name;
   folders[index].parentId = req.body.parentId;
 
-  // add folder reference in new parent folder
+  // Add folder reference in its new parent folder
   var newParentIndex = findIndex(folders[index].parentId);
   folders[newParentIndex].childIds.push(id);
 
@@ -107,34 +133,61 @@ exports.destroy = function(req, res) {
   var id = parseInt(req.params.id);
   var index = findIndex(id);
 
+  // Prevents removing unknown folder and removing the ROOT folder
   if(index == -1) {
     return res.send(404);
   } else if(folders[index].name == "ROOT") {
     return res.send(404);
   }
 
-  // remove folder reference in parent folder
+  // Remove folder reference in its parent folder
   var parentIndex = findIndex(folders[index].parentId);
   folders[parentIndex].childIds.splice(folders[parentIndex].childIds.indexOf(index), 1);
 
-  // remove child folders recursively (if any)
+  // Remove child folders recursively (if any)
   removeChildren(folders[index].childIds)
 
-  folders.splice(index, 1);
+  // Remove folder itself
+  folders.splice(findIndex(id), 1);
 
   return res.send(204);
 };
 
+// Finds the id in the given children (recursively).
+// Returns 1 if the id was found or 0 if not.
+function findIDInChildren(id, children) {
+  for(var i = 0; i < children.length; i++) {
+    // ID was found, return 1
+    if(children[i] == id) {
+      return 1;
+    }
+
+    // Find child's index in folders array to find and search its children
+    var childIndex = findIndex(children[i]);
+    if(childIndex != -1) {
+      return findIDInChildren(id, folders[childIndex].childIds);
+    }
+  }
+
+  // ID not found, return 0
+  return 0;
+};
+
+// Removes the given children from the folders array (recursively)
 function removeChildren(children) {
   for(var i = 0; i < children.length; i++) {
     var childIndex = findIndex(children[i]);
     if(childIndex != -1) {
-      removeChildren(folders[childIndex].childIds);
+      // Get the child's children, remove the child, then remove its children
+      var childIds = folders[childIndex].childIds;
       folders.splice(childIndex, 1);
+      removeChildren(childIds);
     }
   }
 }
 
+// Finds index in folders array based on the given id.
+// Returns the index or -1 if not found.
 function findIndex(id) {
   var index;
 
--- server/api/folder/folder.spec.js
+++ server/api/folder/folder.spec.js
@@ -1,8 +1,48 @@
 'use strict';
 
+// NOTE: The tests must be run in the order shown or it will break the tests.
+
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+
+var rootFolderInitial = {
+      _id: 0,
+      username: "test",
+      name: "ROOT",
+      childIds: []
+    };
+
+var folders = [
+    {
+      username: "test",
+      name: "folderChild",
+      parentId: 0,
+      childIds: []
+    },{
+      username: "test",
+      name: "folderParent",
+      parentId: 0,
+      childIds: [1]
+    },{
+      username: "test",
+      name: "folderGrandparent",
+      parentId: 0,
+      childIds: [2]
+    },
+    {
+      username: "test",
+      name: "folderSibling",
+      parentId: 2,
+      childIds: []
+    },
+    {
+      username: "test",
+      name: "folderGreatGP",
+      parentId: 0,
+      childIds: []
+    }
+];
 
 describe('GET /api/folders', function() {
 
@@ -58,38 +98,6 @@ describe('GET /api/folders/:id', function() {
 
 describe('POST /api/folders', function() {
 
-  var rootFolderInitial = {
-        _id: 0,
-        username: "test",
-        name: "ROOT",
-        childIds: []
-      };
-
-  var folders = [
-      {
-        username: "test",
-        name: "folderChild",
-        parentId: 0,
-        childIds: []
-      },{
-        username: "test",
-        name: "folderParent",
-        parentId: 0,
-        childIds: [1]
-      },{
-        username: "test",
-        name: "folderGrandparent",
-        parentId: 0,
-        childIds: [2]
-      },
-      {
-        username: "test",
-        name: "folderSibling",
-        parentId: 2,
-        childIds: []
-      }
-  ];
-
   it('should not be able to add to other user\'s folders', function(done) {
     request(app)
       .post('/api/folders')
@@ -105,6 +113,48 @@ describe('POST /api/folders', function() {
       .set('user', 'test')
       .set('Content-Type','application/json')
       .send(rootFolderInitial)
+      .expect(404, done);
+  });
+
+  it('should not be able to add folder with self as parent', function(done) {
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderChild",
+        parentId: 1,
+        childIds: []
+      })
+      .expect(404, done);
+  });
+
+  it('should not be able to add folder with unknown parent', function(done) {
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderChild",
+        parentId: 2,
+        childIds: []
+      })
+      .expect(404, done);
+  });
+
+  it('should not be able to add folder with self as child', function(done) {
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderChild",
+        parentId: 1,
+        childIds: []
+      })
       .expect(404, done);
   });
 
@@ -240,6 +290,356 @@ describe('POST /api/folders', function() {
       .end(function(err, res) {
         if (err) return done(err);
         res.body.childIds.should.eql([1, 4]);
+        done();
+      });
+  });
+  
+  it('should not be able to move a folder within its contents', function(done) {
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderGreatGP",
+        parentId: 1,
+        childIds: [2]
+      })
+      .expect(404, done);
+  });
+});
+
+describe('PUT /api/folders/:id', function() {
+
+  it('should not be able to change another user\'s folders', function(done) {
+    request(app)
+      .put('/api/folders/0')
+      .set('user', 'test1')
+      .set('Content-Type','application/json')
+      .send(rootFolderInitial)
+      .expect(404, done);
+  });
+
+  it('should not be able to change ROOT folder', function(done) {
+    request(app)
+      .put('/api/folders/0')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(rootFolderInitial)
+      .expect(404, done);
+  });
+
+  it('should not be able to change ROOT folder\'s name', function(done) {
+    request(app)
+      .put('/api/folders/0')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        _id: 0,
+        username: "test",
+        name: "ROOT2",
+        childIds: []
+      })
+      .expect(404, done);
+  });
+
+  it('should not be able to change a folder to name ROOT', function(done) {
+    // Add new folder
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folders[0])
+      .expect(200);
+
+    // Try to change the new folder's name to ROOT
+    request(app)
+      .put('/api/folders/1')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        name: "ROOT",
+        parentId: 0,
+        childIds: []
+      })
+      .expect(404, done);
+  });
+
+  it('should not be able to change self as its parent', function(done) {
+    request(app)
+      .put('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folders[3])
+      .expect(404, done);
+  });
+
+  it('should not be able to change to unknown parent', function(done) {
+    request(app)
+      .put('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderSibling",
+        parentId: 4,
+        childIds: []
+      })
+      .expect(404, done);
+  });
+
+  it('should not be able to change self as its child', function(done) {
+    request(app)
+      .put('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderSibling",
+        parentId: 2,
+        childIds: [4]
+      })
+      .expect(404, done);
+  });
+
+  it('should return error on unknown id', function(done) {
+    request(app)
+      .put('/api/folders/5')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folders[0])
+      .expect(404, done);
+  });
+
+  it('should change folder name', function(done) {
+    var newName = "folderChild1";
+    var folder = folders[0];
+
+    // Change parentId to match what's in the system and make new name
+    folder.parentId = 2;
+    folder.name = newName;
+
+    // Update with new name
+    request(app)
+      .put('/api/folders/1')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folder)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.name.should.eql(newName);
+        done();
+      });
+  });
+
+  it('should move single folder to another existing folder', function(done) {
+    var folder = folders[3];
+    folder.parentId = 3;
+
+    request(app)
+      .put('/api/folders/4')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folders[3])
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.name.should.eql(folder.name);
+        res.body.parentId.should.eql(folder.parentId);
+      });
+
+    // Check new parent for child folder reference
+    request(app)
+      .get('/api/folders/3')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.containEql(4);
+      });
+
+    // Check old parent for removal of folderSibling in childIds
+    request(app)
+      .get('/api/folders/2')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql(folders[1].childIds);
+        done();
+      });
+  });
+
+  it('should move folder, with new name, and all contents to another folder', function(done) {
+    // Create new folder folderGreatGP
+    request(app)
+      .post('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folders[4])
+      .expect(201)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.containEql(folders[4]);
+      });
+
+    var newName = "folderGrandparent1";
+    var folder = folders[2];
+    folder.parentId = 5;
+    folder.childIds = [2, 4];
+    folder.name = newName;
+
+    // Move folderGrandparent to folderGreatGP and change name to folderGrandparent1
+    request(app)
+      .put('/api/folders/3')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send(folder)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.parentId.should.eql(5);
+        res.body.childIds.should.eql([2, 4]);
+        res.body.name.should.eql(newName);
+      });
+
+    // Check ROOT for change of childIds
+    request(app)
+      .get('/api/folders/0')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([5]);
+      });
+
+    // Check folderGreatGP for new childIds
+    request(app)
+      .get('/api/folders/5')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([3]);
+      });
+
+    // Check folderParent didn't change
+    request(app)
+      .get('/api/folders/2')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([1]);
+        res.body.parentId.should.eql(3);
+      });
+
+    // Check folderSibling didn't change
+    request(app)
+      .get('/api/folders/4')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([]);
+        res.body.parentId.should.eql(3);
+      });
+
+    // Check folderChild didn't change
+    request(app)
+      .get('/api/folders/1')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([]);
+        res.body.parentId.should.eql(2);
+        done();
+      });
+  });
+  
+  it('should not be able to move a folder within its contents', function(done) {
+    request(app)
+      .put('/api/folders')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .send({
+        username: "test",
+        name: "folderGreatGP",
+        parentId: 4,
+        childIds: [3]
+      })
+      .expect(404, done);
+  });
+});
+
+describe('DELETE /api/folders/:id', function() {
+
+  it('should not be able to delete another user\'s folders', function(done) {
+    request(app)
+      .delete('/api/folders/1')
+      .set('user', 'test1')
+      .expect(404, done);
+  });
+
+  it('should not be able to delete ROOT folder', function(done) {
+    request(app)
+      .delete('/api/folders/0')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .expect(404, done);
+  });
+
+  it('should delete single folder that has no children', function(done) {
+    // Delete folderChild
+    request(app)
+      .delete('/api/folders/1')
+      .set('user', 'test')
+      .set('Content-Type','application/json')
+      .expect(204)
+      .end(function(err) {
+        if (err) return done(err);
+      });
+
+    // folderChild shouldn't exist anymore
+    request(app)
+      .get('/api/folders/1')
+      .set('user', 'test')
+      .expect(404);
+
+    // Check folderParent doesn't have children anymore
+    request(app)
+      .get('/api/folders/2')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.childIds.should.eql([]);
+        done();
+      });
+  });
+
+  it('should delete everything', function(done) {
+    // Delete folderGreatGP
+    request(app)
+      .delete('/api/folders/5')
+      .set('user', 'test')
+      .expect(204)
+      .end(function(err, res) {
+        if (err) return done(err);
+      });
+
+    // Only ROOT should exist in the system now
+    request(app)
+      .get('/api/folders')
+      .set('user', 'test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.have.lengthOf(1);
+        res.body[0].childIds.should.eql([]);
+        res.body[0].name.should.eql("ROOT");
         done();
       });
   });
