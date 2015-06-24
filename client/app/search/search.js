b6809944ddbc18e54e8a370ecaa881dd5e5d5f8a
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-06-24 17:09:29 -0400

    Starting with simplest case -- processing a queryString that is part of a URL parameter.

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -20,7 +20,7 @@ angular.module('digApp')
         controller: 'SearchResultsCtrl'
     })
     .state('search.results.list', {
-        url: '^/list',
+        url: '^/list?queryString',
         templateUrl: 'app/search/search-results/list/list.partial.html',
 		preserveScrollPos: true
       })
