89f9f8b9717bb1be4dd1eac125a637a1bf07e104
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-07-29 17:39:18 -0400

    Moving posts display to component to avoid code duplication.

---
+++ client/components/thread-view/post-template.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('digApp.directives')
+.directive('postTemplate', function(euiConfigs, textHighlightService) {
+    return {
+        restrict: 'E',
+        scope: {
+            post: '='
+        },
+        templateUrl: 'components/thread-view/post-template.partial.html',
+        link: function($scope) {
+            $scope.euiConfigs = euiConfigs;
+
+            $scope.fieldIsArray = function(field) {
+                return angular.isArray(field);
+            };
+
+            $scope.highlightCheck = function(field, highlightedText) {
+                return textHighlightService.highlightCheck(field, highlightedText);
+            };
+        }
+    };
+});
