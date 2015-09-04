6b8c9f2e28ccc22a4bea273b8208ccf071c5c4fa
Rachel Artiss <rachel.artiss@nextcentury.com>
2015-09-04 14:47:22 -0400

    Patent accordion changes.

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -336,7 +336,8 @@ var all = {
                             title: 'Plaintiff',
                             field: 'plaintiff.name',
                             classes: 'plaintiff'
-                        },{
+                        }],
+                        "3":[{
                             title: 'Defendant',
                             field: 'defendant.name',
                             classes: 'defendant'
