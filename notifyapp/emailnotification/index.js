237ee9cd2da8a3f5c2a267336060dd563f5330aa
flyntde <dflyntgit@gmail.com>
2015-09-28 12:11:07 -0400

    replace sendgrid client with emailjs smtp client

--- notifyapp/emailnotification/index.js
+++ notifyapp/emailnotification/index.js
@@ -2,28 +2,30 @@
 
 'use strict';
 
-var SendGrid   = require('sendgrid');
+var email = require('emailjs');
 
-module.exports = function NotificationEmail (fromAddr, digUrl, smtpUser, smtpPass) {
+module.exports = function NotificationEmail (fromAddr, digUrl, smtpHost, smtpUser, smtpPass) {
     this.fromAddr = fromAddr;
     this.digUrl = digUrl;
+    this.smtpHost = smtpHost;
     this.smtpUser = smtpUser;
     this.smtpPass = smtpPass;
 
-    // create new sendgrid object that knows how to connect to the SendGrid remote service API
-    var sendgrid = new SendGrid(this.smtpUser, this.smtpPass);
-
-    // send the following message with sendgrid client.  SendGrid will call callback
-    // function with two parameters: error (a string) and message (json object).
     this.send = function (user, query, cb) {
-        sendgrid.send({
-            to: user.emailAddress,
-            from: this.fromAddr,
+        var server  = email.server.connect({
+           user: this.smtpUser, 
+           password: this.smtpPass, 
+           host: this.smtpHost, 
+           ssl: true
+        });
+
+        server.send({
             subject: 'DIG Notification: ' + query.name,
             text: 'There are new results available for the DIG saved query ' + 
             query.name + '.\n' +
-            'Notification results can be accessed in the DIG application here: ' + digUrl
+            'Notification results can be accessed in the DIG application here: ' + digUrl,
+            from: this.fromAddr,
+            to: user.emailAddress
         }, cb);
     };
 };
-
